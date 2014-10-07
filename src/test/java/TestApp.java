cf0b8553d67ed5ce35db253717a199f62560c45d
buildsample <buildsampletest@gmail.com>
2014-10-07 10:44:53 +0530

    Update TestApp.java

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -8,7 +8,7 @@ public class TestApp extends TestCase {
       App app = new App();
       result = app.getData();
     } finally {
-      assertEquals("Hope thiworks!", result);
+      assertEquals("Hope this works!", result);
     }
   } 
 }
